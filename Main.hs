2c158aa5a07829a7f0e6c9cb5ff2a049c048d1ae
bryan <bryanfeliciano892@gmail.com>
2022-04-27 10:16:19 -0500

    included all IO in a single Main file

--- Main.hs
+++ Main.hs
@@ -1,9 +1,26 @@
-helloPerson :: String -> String
-helloPerson name = "Hello " ++ name ++ " ! "
+import PizzaFile
 
 main :: IO()
 main = do
-    putStrLn "Hello! what's your name?"
-    name <- getLine
-    let statement = helloPerson name
-    putStrLn statement
+    putStrLn "what is the size of pizza 1 ? "
+    size1 <- getLine
+    putStrLn "what is the cost of pizza 1 ? "
+    cost1 <- getLine
+    putStrLn "what is the size of pizza 2 ? "
+    size2 <- getLine
+    putStrLn "what is the cost of pizza 2 ? "
+    cost2 <- getLine
+    let pizza1 = (read size1,read cost1)
+    let pizza2 = (read size2,read cost2)
+    let betterPizza = comparePizzas pizza1 pizza2
+    putStrLn (describePizza betterPizza)
+
+-- helloPerson :: String -> String
+-- helloPerson name = "Hello " ++ name ++ " ! "
+
+-- main :: IO()
+-- main = do
+--     putStrLn "Hello! what's your name?"
+--     name <- getLine
+--     let statement = helloPerson name
+--     putStrLn statement
